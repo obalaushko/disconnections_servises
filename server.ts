4df70797d174b352e09ec07efc259d411b642640
Deimos <alex279368@gmail.com>
2024-09-29 14:51:41 +0300

    Add last success result

--- server.ts
+++ server.ts
@@ -1,16 +1,24 @@
 import { scrapeTable } from "./scrape";
 
 const port = 3000;
+let lastSuccsessResult: object;
 
 const server = Bun.serve({
   port,
   async fetch(req) {
     const result = await scrapeTable();
-	console.log(result)
-    return Response.json(result);
+    console.log(result);
+
+    if (result) {
+      lastSuccsessResult = result;
+      return Response.json(result);
+    } else {
+      console.warn("Return last success result", lastSuccsessResult);
+      return Response.json(lastSuccsessResult);
+    }
   },
   error(error) {
-	console.error('Bun server Error', error);
+    console.error("Bun server Error", error);
     return new Response(`<pre>${error}\n${error.stack}</pre>`, {
       headers: {
         "Content-Type": "text/html",
